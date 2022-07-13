b2e13dd2717d8a3ac777e49caffdce97d6e62f2e
ManuelPinilla <mapinillam@unal.edu.co>
2022-07-13 18:00:42 +0000

    ver2

--- Excel/index.py
+++ Excel/index.py
@@ -2,27 +2,28 @@ import pandas as pd
 from openpyxl.chart import BarChart , Reference
 from openpyxl import load_workbook
 
-archivo = pd.read_excel("Excel/supermarket.xlsx")
+archivo = pd.read_excel("Excel/supermarket.xlsx") # leer archivo xlsx
 columnas = archivo[["Gender","Product line","Total"]] #Filtar solo las columanas que se usan
 
-tabla_dinamica = columnas.pivot_table(index="Gender",columns="Product line",values="Total",aggfunc="sum").round(0)
-
-tabla_dinamica.to_excel("ventas2021.xlsx", startrow=4, sheet_name="Reporte" )
+tabla_dinamica = columnas.pivot_table(index="Gender",columns="Product line",values="Total",aggfunc="sum").round(0) #crear una tabla dinamica 
 
 
-wb = load_workbook("ventas2021.xlsx")
+tabla_dinamica.to_excel("ventas2021.xlsx", startrow=4, sheet_name="Reporte" ) # guardar el excel
 
-pestaña = wb["Reporte"]
+
+wb = load_workbook("ventas2021.xlsx") # mantener el libro abierto para modificarlo
+
+pestaña = wb["Reporte"] # pestaña a la que se accederá
 
 min_col= wb.active.min_column
 max_col= wb.active.max_column
 min_fil= wb.active.min_row
 max_fil=wb.active.max_row
+#referencia de los puntos maximos y minimos de la tabla
 
+grafico = BarChart() # creacion de un bartchart
 
-grafico = BarChart()
-
-datos = Reference(pestaña, min_col= min_col+1, max_col=max_col, min_row=min_fil, max_row=max_fil)
+datos = Reference(pestaña, min_col= min_col+1, max_col=max_col, min_row=min_fil, max_row=max_fil) 
 
 categorias = Reference(pestaña, min_col= min_col, max_col=min_col, min_row=min_fil+1 , max_row=max_fil)
 
